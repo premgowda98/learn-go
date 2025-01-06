cddde6d775c447eaedeb9ed1e719285da689a3aa
Prem Kumar <premkumarmvks@gmail.com>
2025-01-06 18:50:25 +0530

    gopherecises

--- gophercises/quiz/main.go
+++ gophercises/quiz/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main(){
+	data, err := readCSVFile("./problems.csv")
+
+	if err !=nil{
+		fmt.Println("Failed to read data")
+	}
+
+	fmt.Println(data)
+	
+}
+
+func readCSVFile(filename string)([]byte, error){
+	f, err := os.Open(filename)
+
+	if err !=nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+
+	if err !=nil {
+		return nil, err
+	}
+
+	return data, nil
+
+}
