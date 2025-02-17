dc68e9556e71ef898b013815cec8321e87342bff
Prem Kumar G <premkumarmvks@gmail.com>
2025-02-17 21:54:18 +0530

    project compelete

--- projects/car-zone/db/postgres.go
+++ projects/car-zone/db/postgres.go
@@ -1,1 +1,63 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func InitDB() (*sql.DB, error) {
+	conString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
+	db, err := sql.Open("postgres", conString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = createTable(db)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func createTable(db *sql.DB) error {
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS engine (
+		id UUID PRIMARY KEY,
+		displacement INT NOT NULL,
+		cylinders INT NOT NULL,
+		range INT NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);
+
+	CREATE TABLE IF NOT EXISTS car (
+		id UUID PRIMARY KEY,
+		name VARCHAR(255) NOT NULL,
+		year VARCHAR(4) NOT NULL,
+		brand VARCHAR(255) NOT NULL,
+		fuel_type VARCHAR(50) NOT NULL,
+		engine UUID NOT NULL,
+		price DECIMAL(10, 2) NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);`)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- projects/car-zone/main.go
+++ projects/car-zone/main.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"project/car-zone/db"
+	"project/car-zone/store/car"
+	"project/car-zone/store/engine"
+
+	carService "project/car-zone/service/car"
+	engineService "project/car-zone/service/engine"
+
+	carHandler "project/car-zone/handler/car"
+	engineHandler "project/car-zone/handler/engine"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := db.InitDB()
+
+	if err != nil {
+		log.Fatalf("failed initialise database %v", err)
+	}
+
+	carStore := car.New(db)
+	engineStore := engine.New(db)
+
+	carService := carService.New(carStore)
+	engineService := engineService.New(engineStore)
+
+	carHandler := carHandler.New(carService)
+	engineHandler := engineHandler.New(engineService)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/cars/{id}", carHandler.GetCarById).Methods("GET")
+	router.HandleFunc("/cars", carHandler.GetCarByBrand).Methods("GET")
+	router.HandleFunc("/cars", carHandler.CreateCar).Methods("POST")
+
+	router.HandleFunc("/engine/{id}", engineHandler.GetEngineById).Methods("GET")
+	router.HandleFunc("/engine", engineHandler.CreateEngine).Methods("POST")
+
+	log.Println("Server Running on port 8080")
+	http.ListenAndServe(":8080", router)
+}
